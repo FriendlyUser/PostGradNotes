7879dc5af774819395f3ec260bb214038b2ab2fd
dli <davidli012345@gmail.com>
2020-06-23 21:25:21 -0700

    updating with spacy commits

---
+++ chapters/2020/spacy/ch1/first_token.py
@@ -0,0 +1,14 @@
+# Import the English language class and create the nlp object
+from spacy.lang.en import English
+
+# Create the nlp object
+nlp = English()
+
+# Process the text
+doc = nlp("I like tree kangaroos and narwhals.")
+
+# Select the first token
+first_token = doc[0]
+
+# Print the first token's text
+print(first_token.text)
